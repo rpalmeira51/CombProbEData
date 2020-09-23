2a2f58f7e86adb31ffbafbef6648ced0cdabc00c
Rodrigo Palmeira <rpalmeira1999@gmail.com>
2020-09-22 22:43:53 -0300

    bfs

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import os 
+from collections import deque 
 
 def generateRandomGraph(size):
-    graph = [[0]*size]*size
+    #graph = [[0]*size]*size
+    graph = [ [ 0 for y in range( size ) ] for x in range( size ) ]
     for i in range(size):
-        for j in range(size):
+        for j in range(i+1,size):
             p = int.from_bytes(os.urandom(4),"big")
+            #print(p)
             if(p >= 2**31):
-                graph[i][j] = 0
-            else:
                 graph[i][j] = 1
+                graph[j][i] = 1
+
     return graph
 
-g= generateRandomGraph(10)
+def bfs(graph):
+    q = deque()
+    q.append(0)
+    component = [0]
+    color = {0:True}
+    while (q):
+        v = q.popleft()
+        for i in range(len(graph)):
+            #print(i)
+            if(graph[v][i] ==1 and (i not in color)):
+                print(i)
+                q.append(i)
+                component.append(i)
+                color[i] = True
+    return component
+
+
+g= generateRandomGraph(5)
 print(g)
+print(bfs(g))
